f29099873bd869dccd6c090bab16b329531c8d39
Andrew V. Adinetz <adinetz@gmail.com>
2013-07-05 23:41:01 +0200

    added latency test and CUDA malloc throughput test. Latency is 4 us, and halloc is 1000x faster than CUDA allocator

--- test.cu
+++ test.cu
@@ -30,7 +30,7 @@ void ha_shutdown(void);
 
 /** allocator parameters */
 #define BLOCK_SZ 16
-#define NBLOCKS (16 * 1024 * 1024)
+#define NBLOCKS (8 * 1024 * 1024)
 #define HASH_STEP (NBLOCKS / 256 + NBLOCKS / 64 - 1)
 //#define HASH_STEP (NBLOCKS / (64) - 1)
 #define NCOUNTERS 2048
@@ -39,11 +39,13 @@ void ha_shutdown(void);
 //#define NCOUNTERS 2048
 
 /** testing parameters */
-#define NTHREADS (8 * 1024 * 1024)
+#define NTHREADS (2 * 1024 * 1024)
+#define LAT_NMALLOCS (32 * 1024)
+//#define NTHREADS 16384
 #define NMALLOCS 16
 #define NTHREADS2 (NTHREADS / NMALLOCS)
 #define BS 512
-#define NTRIES 128
+#define NTRIES 8
 //#define NTRIES 1
 
 /** the buffer from which to allocate memory, [nblocks_g * block_sz_g] bytes */
@@ -222,28 +224,47 @@ void ha_shutdown(void) {
 }  // ha_shutdown
 
 // alloc/free kernel
-__global__ void malloc_free_k(int n) {
-	void *p = hamalloc(16);
-	hafree(p);
+__global__ void malloc_free_k(int ntimes) {
+	for(int i = 0; i < ntimes; i++) {
+		void *p = hamalloc(16);
+		hafree(p);
+	}
 }  // malloc_free_k
 
-// alloc--and-save-pointer kernel
-__global__ void malloc_k(void **ptrs) {
+// alloc-and-save-pointer kernel
+__global__ void malloc_k(void **ptrs, int ntimes) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	for(int iptr = tid; iptr < NMALLOCS * NTHREADS2; iptr += NTHREADS2)
+	int nthreads = blockDim.x * gridDim.x;
+	for(int iptr = tid; iptr < ntimes * nthreads; iptr += nthreads)
 		ptrs[iptr] = hamalloc(16);
 }  // malloc_k
 // read-and-free pointer kernel
-__global__ void free_k(void **ptrs) {
+__global__ void free_k(void **ptrs, int ntimes) {
 	int tid = threadIdx.x + blockIdx.x * blockDim.x;
-	for(int iptr = tid; iptr < NMALLOCS * NTHREADS2; iptr += NTHREADS2)
+	int nthreads = blockDim.x * gridDim.x;
+	for(int iptr = tid; iptr < ntimes * nthreads; iptr += nthreads)
 		hafree(ptrs[iptr]);
+}  // free_k
+
+// alloc-and-save-pointer kernel
+__global__ void cuda_malloc_k(void **ptrs, int ntimes) {
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int nthreads = blockDim.x * gridDim.x;
+	for(int iptr = tid; iptr < ntimes * nthreads; iptr += nthreads)
+		ptrs[iptr] = malloc(16);
+}  // malloc_k
+// read-and-free pointer kernel
+__global__ void cuda_free_k(void **ptrs, int ntimes) {
+	int tid = threadIdx.x + blockIdx.x * blockDim.x;
+	int nthreads = blockDim.x * gridDim.x;
+	for(int iptr = tid; iptr < ntimes * nthreads; iptr += nthreads)
+		free(ptrs[iptr]);
 }  // free_k
 
 void run_test1(void) {
 	double t1 = omp_get_wtime();
 	for(int itry = 0; itry < NTRIES; itry++) {
-		malloc_free_k<<<NTHREADS / BS, BS>>>(0);
+		malloc_free_k<<<NTHREADS / BS, BS>>>(1);
 		cucheck(cudaGetLastError());
 		cucheck(cudaStreamSynchronize(0));
 	}
@@ -263,10 +284,10 @@ void run_test2(void) {
 	cucheck(cudaMemset(d_ptrs, 0, ptrs_sz));
 	double t1 = omp_get_wtime();
 	for(int itry = 0; itry < NTRIES; itry++) {
-		malloc_k<<<NTHREADS2 / BS, BS>>>(d_ptrs);
+		malloc_k<<<NTHREADS2 / BS, BS>>>(d_ptrs, NMALLOCS);
 		cucheck(cudaGetLastError());
 		//cucheck(cudaStreamSynchronize(0));
-		free_k<<<NTHREADS2 / BS, BS>>>(d_ptrs);
+		free_k<<<NTHREADS2 / BS, BS>>>(d_ptrs, NMALLOCS);
 		cucheck(cudaGetLastError());
 		cucheck(cudaStreamSynchronize(0));
 	}
@@ -275,8 +296,8 @@ void run_test2(void) {
 	void **h_ptrs = (void **)malloc(ptrs_sz);
 	cucheck(cudaMemcpy(h_ptrs, d_ptrs, ptrs_sz, cudaMemcpyDeviceToHost));
 	cucheck(cudaFree(d_ptrs));
-	for(int ip = 0; ip < 256; ip += 7)
-		printf("d_ptrs[%d] = %p\n", ip, h_ptrs[ip]);
+	//for(int ip = 0; ip < 256; ip += 7)
+	//	printf("d_ptrs[%d] = %p\n", ip, h_ptrs[ip]);
 	free(h_ptrs);
 	double nmallocs = (double)NMALLOCS * NTHREADS2 * NTRIES;
 	printf("test 2 (first all mallocs, then all frees):\n");
@@ -286,9 +307,60 @@ void run_test2(void) {
 	printf("\n");
 }  // run_test2
 
+/** latency test */
+void run_test3(void) {
+	double t1 = omp_get_wtime();
+	for(int itry = 0; itry < NTRIES; itry++) {
+		malloc_free_k<<<1, 1>>>(LAT_NMALLOCS);
+		cucheck(cudaGetLastError());
+		cucheck(cudaStreamSynchronize(0));
+	}
+	double t2 = omp_get_wtime();
+	double nmallocs = (double)LAT_NMALLOCS * NTRIES;
+	printf("test 3 (latency):\n");
+	printf("test duration %.3lf ms\n", (t2 - t1) * 1e3);
+	printf("%.0lf malloc/free pairs in the test\n", nmallocs);
+	printf("latency: %.0lf ns\n", (t2 - t1) * 1e9 / nmallocs);
+	printf("\n");
+}  // run_test3
+
+/** throughput test for CUDA allocator */
+void run_test4(void) {
+	void **d_ptrs;
+	int cuda_nthreads = 32768, cuda_nmallocs = 2, cuda_ntries = 4;
+	size_t ptrs_sz = cuda_nthreads * cuda_nmallocs * sizeof(void *);
+	cucheck(cudaMalloc(&d_ptrs, ptrs_sz));
+	cucheck(cudaMemset(d_ptrs, 0, ptrs_sz));
+	double t1 = omp_get_wtime();
+	for(int itry = 0; itry < cuda_ntries; itry++) {
+		cuda_malloc_k<<<cuda_nthreads / BS, BS>>>(d_ptrs, cuda_nmallocs);
+		cucheck(cudaGetLastError());
+		//cucheck(cudaStreamSynchronize(0));
+		cuda_free_k<<<cuda_nthreads / BS, BS>>>(d_ptrs, cuda_nmallocs);
+		cucheck(cudaGetLastError());
+		cucheck(cudaStreamSynchronize(0));
+	}
+	double t2 = omp_get_wtime();
+	// copy back and print some allocated pointers
+	void **h_ptrs = (void **)malloc(ptrs_sz);
+	cucheck(cudaMemcpy(h_ptrs, d_ptrs, ptrs_sz, cudaMemcpyDeviceToHost));
+	cucheck(cudaFree(d_ptrs));
+	//for(int ip = 0; ip < 256; ip += 7)
+	//	printf("d_ptrs[%d] = %p\n", ip, h_ptrs[ip]);
+	free(h_ptrs);
+	double nmallocs = (double)cuda_nmallocs * cuda_nthreads * cuda_ntries;
+	printf("test 4 (CUDA, first all mallocs, then all frees):\n");
+	printf("test duration %.3lf ms\n", (t2 - t1) * 1e3);
+	printf("%.0lf malloc/free pairs in the test\n", nmallocs);
+	printf("allocation speed: %.3lf Mpairs/s\n", nmallocs / (t2 - t1) * 1e-6);
+	printf("\n");
+}  // run_test4
+
 int main(int argc, char **argv) {
 	ha_init();
 	run_test1();
 	run_test2();
+	run_test3();
+	run_test4();
 	ha_shutdown();
 }  // main
