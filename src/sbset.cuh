8129ccaeeb87829f67caaff9de87d2212e5bbb57
Andrew V. Adinetz <adinetz@gmail.com>
2013-08-19 10:44:46 +0200

    slight modification to sbset search (now using __ffs)

--- src/sbset.cuh
+++ src/sbset.cuh
@@ -5,26 +5,15 @@
 __device__ inline uint sbset_get_from(sbset_t sbset) {
 	// TODO: maybe do several trials to be sure
 	for(uint iword = 0; iword < nsbs_g / WORD_SZ; iword++) {
-		uint word = sbset[iword];
-		if(word) {
-			for(uint ibit = 0; ibit < WORD_SZ; ibit++)
-				if((word >> ibit) & 1) {
-					// try locking the bit
-					uint mask = 1 << ibit;
-					if(atomicAnd(&sbset[iword], ~mask) & mask)
-						return iword * WORD_SZ + ibit;
-				}
-		}
-		/*
-			if(word) {
-			for(uint ibit = __ffs(word) - 1; word;
-			word &= ~(1 <<	ibit), ibit = __ffs(word) - 1) {
+		uint word = *(volatile uint *)&sbset[iword];
+		while(word) {
+			uint ibit = __ffs(word) - 1;
 			// try locking the bit
 			uint mask = 1 << ibit;
 			if(atomicAnd(&sbset[iword], ~mask) & mask)
-			return iword * WORD_SZ + ibit;
-			}
-			}*/
+				return iword * WORD_SZ + ibit;
+			word &= ~mask;
+		}
 	}
 	return SB_NONE;
 }  // sbset_get_from
